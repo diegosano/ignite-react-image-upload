ed046316926bbcb9f3dffe62bcca8f49731f2c9f
Diego Sano <diego.sano@nave.rs>
2022-05-06 11:03:48 -0300

    feat: view image modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,21 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+
+      <ModalContent w="auto" h="auto">
+        <ModalBody p={0}>
+          <Image src={imgUrl} maxH="600px" maxW="900px" />
+        </ModalBody>
+
+        <ModalFooter bg="pGray.800">
+          <Link href={imgUrl} isExternal mr="auto" color="pGray.50">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
